84c9d7a243492ffaabf042d182a487e179d002a1
ItaloSaid <italo13said@gmail.com>
2023-07-24 05:12:10 -0300

    Adicionando arquivo bin

---
+++ README.md
@@ -0,0 +1,3 @@
+#Documentação do componente
+
+componente  bla bla bla
--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,3 +1,4 @@
+#!/usr/bin/env php
 <?php
 
 require 'vendor/autoload.php';
@@ -15,5 +16,5 @@ $crawler = new Crawler();
 $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar("cursos-online-programacao/php");
 foreach ($cursos as $curso) {
-    echo exibeMensagem($curso);
+    exibeMensagem($curso);
 }
